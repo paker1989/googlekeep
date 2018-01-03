bebeb54a31ca3831a2b8bcfbb60d554e62a2a8dc
parker1989 <xubinqz@gmail.com>
2018-01-03 23:58:01 +0100

    commit

---
+++ src/components/headerBar.vue
@@ -0,0 +1,54 @@
+<template lang="jade">
+  .headerBar
+    .leftComponent
+      span.glyphicon.glyphicon-list
+      .logo
+        strong Google
+        | keep
+    .rightComponent
+
+</template>
+<script>
+export default {
+  name: 'headerBar',
+  data() {
+    return {
+
+    }
+  },
+}
+</script>
+<style lang="less" scoped>
+@import (reference) '../style/headerVars';
+.headerBar {
+  .headerLayout;
+  background-color: @bg-color;
+  width: 100%;
+  min-height: @min-height;
+  padding: 8px 20px;
+
+  & .leftComponent {
+    .headerLayout(space-around);
+    & > * {
+      margin-right: 20px;
+    }
+    & .logo {
+      font-size: 22px;
+      & strong {
+        margin-right:5px;
+      }
+    }
+  }//end .leftComponent
+
+}//end .headerBar
+
+.headerLayout(@justifyContent: space-between) {
+  position: relative;
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: @justifyContent;
+  align-items: center;
+}
+</style>
+
+
---
+++ src/views/home.vue
@@ -0,0 +1,26 @@
+<template lang="jade">
+  .home.container-fluid
+    header-bar
+</template>
+<script>
+import HeaderBar from '@/components/headerBar'
+
+export default {
+  name: 'home',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    HeaderBar,
+  }
+}
+</script>
+<style lang="less" scoped>
+  .container-fluid {
+    padding:0;
+  }
+</style>
+
+
