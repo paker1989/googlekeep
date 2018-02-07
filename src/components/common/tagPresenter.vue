8ac3be735d85e626ffcd010e80148c34e2cbfbe7
paker1989 <xubinqz@gmail.com>
2018-02-07 20:38:25 +0100

    commit

---
+++ src/components/common/tagPresenter.vue
@@ -0,0 +1,29 @@
+<template lang="jade">
+  .tagPresenter
+     
+</template>
+<script>
+export default {
+  name: 'tagPresenter',
+  props: {
+    maxDisplayItems: {
+      type: Number,
+      default: -1 // meaning no limitation
+    },
+    selectedTags: {
+      type: Array,
+      default() { return [] }
+    }
+  },
+  created() {
+    
+  }
+}
+</script>
+<style lang="less" scoped>
+@import (reference) '../../style/headerVars';
+.tagPresenter {
+  .headerLayout(flex-start);
+  & .
+}
+</style>
